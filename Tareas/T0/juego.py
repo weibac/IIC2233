a0c568166a71f5fc1b276f5643341b4ebc399a3a
weibac <milan.weibel@uc.cl>
2022-08-25 14:34:55 -0400

    af2 y gitignore

---
+++ Tareas/T0/juego.py
@@ -0,0 +1,13 @@
+from parametros import PROB_BESTIA, POND_PUNT
+from math import ceil
+from random import shuffle
+
+def crear_tablero(dimensiones):
+    x, y = dimensiones
+    casillas = x * y
+    bestias = ceil(casillas * PROB_BESTIA)
+    tablero_lineal = ['N' for a in range(bestias)] + [' ' for a in range(casillas - bestias)]
+    print(tablero_lineal)
+    shuffle(tablero_lineal)
+    tablero = [tablero_lineal[a * x:(a + 1) * x] for a in range(y)]
+    return tablero
