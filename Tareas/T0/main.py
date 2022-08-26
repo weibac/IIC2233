9ea70a908dc4b8764bf035bd1abb720221716922
weibac <milan.weibel@uc.cl>
2022-08-26 14:53:34 -0400

    opcion salir

--- Tareas/T0/main.py
+++ Tareas/T0/main.py
@@ -1,8 +1,9 @@
-import tablero, menus, juego
+import tablero, menus, juego, sys
 
 inp = menus.menu_inicio()
 if inp == 1:
-    
     game = juego.Partida(*menus.nueva_partida())
     tablero.print_tablero(game.tablero_real)
     
+elif inp == 0:
+    sys.exit('\nVuelve pronto!')
