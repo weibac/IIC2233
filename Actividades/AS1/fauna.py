aa5489725b5687fae19ca0eff7f423818ef700ce
weibac <milan.weibel@uc.cl>
2022-09-01 15:10:04 -0400

    fauna init

--- Actividades/AS1/fauna.py
+++ Actividades/AS1/fauna.py
@@ -2,21 +2,24 @@ from random import randint
 from parametros import INCREMENTO_FEROCIDAD, MAX_EX_CARNIVORO, MAX_EX_HERVIBORO, \
                         MIN_EX_CARNIVORO, MIN_EX_HERVIBORO, INCREMENTO_ADORABILIDAD, \
                         GAN_CARNIVORO, GAN_HERBIVORO
-
+from abc import ABC, abstractmethod
 
 
 # MODIFICAR
-class Animal:
+class Animal(ABC):
 
     # MODIFICAR
-    def __init__(self, especie):
-        pass
+    def __init__(self, especie: str, **kwargs):  # TODO: Tal vex kwargs se van
+        self.especie = especie
+        self.ganancia_actual = 0
 
     # MODIFICAR
+    @abstractmethod
     def alimentarse(self):
         pass
 
     # MODIFICAR
+    @abstractmethod
     def exhibicion(self):
         pass
 
@@ -25,49 +28,60 @@ class Animal:
 
 
 # MODIFICAR
-class Carnivoro:
+class Carnivoro(Animal):
 
     # MODIFICAR
-    def __init__(self, ferocidad, **kwargs):
-        pass
-
-    # MODIFICAR
-    def alimentarse(self):
-        super().alimentarse()
-
-    # MODIFICAR
-    def exhibicion(self):
-        super().exhibicion()
-
-
-# MODIFICAR
-class Herbivoro:
-
-    # MODIFICAR
-    def __init__(self, adorabilidad, **kwargs):
-        pass
+    def __init__(self, ferocidad: int, **kwargs):
+        super().__init__(**kwargs) # TODO : tal vez malo
+        self.ferocidad = ferocidad
+        self.ganancia_actual += GAN_CARNIVORO
 
     # MODIFICAR
     def alimentarse(self):
         super().alimentarse()
+        self.ferocidad += INCREMENTO_FEROCIDAD
+        print(f"Animal {self.especie} se come un kilogramo de Carne")
 
     # MODIFICAR
     def exhibicion(self):
         super().exhibicion()
+        self.ganancia_actual += self.ferocidad * randint(MIN_EX_CARNIVORO, MAX_EX_CARNIVORO)
 
 
 # MODIFICAR
-class Omnivoro:
+class Herbivoro(Animal):
+
+    # MODIFICAR
+    def __init__(self, adorabilidad: int, **kwargs):
+        super().__init__(**kwargs)  # TODO : tal vez malo
+        self.adorabilidad = adorabilidad
+        self.ganancia_actual += GAN_HERBIVORO
+
+    # MODIFICAR
+    def alimentarse(self):
+        super().alimentarse()
+        self.adorabilidad += INCREMENTO_ADORABILIDAD
+        print(f"Animal {self.especie} se come un kilogramo de vegetales")
+
+
+    # MODIFICAR
+    def exhibicion(self):
+        super().exhibicion()
+        self.ganancia_actual += self.adorabilidad * randint(MIN_EX_HERVIBORO, MAX_EX_HERVIBORO)
+
+
+# MODIFICAR
+class Omnivoro(Carnivoro, Herbivoro):
 
     # MODIFICAR
     def __init__(self, **kwargs):
-        pass
+        super().__init__(**kwargs)
 
     # MODIFICAR
     def alimentarse(self):
-        pass
+        super().alimentarse()
 
     # MODIFICAR
     def exhibicion(self):
-        pass
+        super().exhibicion()
 
