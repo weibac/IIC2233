e0eaff35c987d710cf18a2cf3d698e7980a8ace2
weibac <milan.weibel@uc.cl>
2022-09-07 17:52:11 -0300

    init carga archivos

---
+++ Tareas/T1/archivos.py
@@ -0,0 +1,21 @@
+import os
+from typing import List
+
+
+def cargar_archivo(ruta_ls: list) -> List[dict]:
+    """
+    Implementa lectura dinámica de CSV
+    """
+    lista_instancias = []
+    ruta = os.path.join(ruta_ls)
+    with open(ruta, mode='r', encoding='utf-8') as archivo:
+        lines = [line.strip('\n').split(',') for line in archivo.readlines()]
+        for a in range(len(lines)):
+            for b in range(len(lines[0])):
+                if ';' in lines[a][b]:
+                    lines[a][b] = lines[a][b].split(';')
+        labels = lines[0]
+        for a in range(1, len(lines)):
+            atributos_instancia = {labels[b]: lines[a][b] for b in range(len(labels))}
+            lista_instancias.append(atributos_instancia)
+    return lista_instancias
