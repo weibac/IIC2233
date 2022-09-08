0520e327a59f130aa1dff6348a40820980450b8b
weibac <milan.weibel@uc.cl>
2022-09-08 11:59:10 -0300

    pasar bases datos archivos a dict por nombre

--- Tareas/T1/archivos.py
+++ Tareas/T1/archivos.py
@@ -1,21 +1,32 @@
 import os
-from typing import List
+from typing import List, Dict
 
 
-def cargar_archivo(ruta_ls: list) -> List[dict]:
+def archivo_a_lista(ruta_ls: tuple) -> List[Dict[str, str]]:
     """
     Implementa lectura dinámica de CSV
     """
-    lista_instancias = []
     ruta = os.path.join(*ruta_ls)
     with open(ruta, mode='r', encoding='utf-8') as archivo:
-        lines = [line.strip('\n').split(',') for line in archivo.readlines()] # TODO: tal vez deindent linea abajo
+        lines = [line.strip('\n').split(',') for line in archivo.readlines()]
     for a in range(len(lines)):
         for b in range(len(lines[0])):
             if ';' in lines[a][b]:
                 lines[a][b] = lines[a][b].split(';')
     labels = lines[0]
+    lista_instancias = []
     for a in range(1, len(lines)):
         atributos_instancia = {labels[b]: lines[a][b] for b in range(len(labels))}
         lista_instancias.append(atributos_instancia)
     return lista_instancias
+
+
+def lista_a_dict(lista_instancias: List[Dict[str, str]]) -> Dict[str, Dict[str, str]]:
+    dict_instancias = dict()
+    for a in range(len(lista_instancias)):
+        dict_instancias[lista_instancias[a]['nombre']] = lista_instancias[a]  # TODO: es hardcoding?
+    return dict_instancias
+
+
+def cargar_archivo(ruta_ls: tuple) -> Dict[str, Dict[str, str]]:
+    return lista_a_dict(archivo_a_lista(ruta_ls))
