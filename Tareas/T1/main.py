c638213f5f283b150b119821aa93a88febd57160
weibac <milan.weibel@uc.cl>
2022-09-14 17:23:40 -0300

    partial: flujo principal y menus

--- Tareas/T1/main.py
+++ Tareas/T1/main.py
@@ -18,20 +18,76 @@ def setup():
     liga = LigaProgramon(datos)
 
     # Crear menus fijos
-    menu_entrenador = Menu(HEADER_MENU_ENTRENADOR, OPCIONES_MENU_ENTRENADOR)
-    menu_objetos = Menu(HEADER_MENU_OBJETOS, OPCIONES_MENU_OBJETOS)
-
-    opciones_menu_inicio = []
+    opciones_menu_inicio = []  # TODO: tal vez cargar desde datos en vez de liga
     for entrenador in liga.entrenadores:
         nombres_programones = [programon.nombre for programon in entrenador.programones]
         opcion = f'{entrenador.nombre}: {", ".join(nombres_programones)}'
         opciones_menu_inicio.append(opcion)
     menu_inicio = Menu(HEADER_MENU_INICIO, opciones_menu_inicio)
 
+    menu_entrenador = Menu(HEADER_MENU_ENTRENADOR, OPCIONES_MENU_ENTRENADOR)
+    menu_objetos = Menu(HEADER_MENU_OBJETOS, OPCIONES_MENU_OBJETOS)
+    MenusTuple = namedtuple('menus', ['inicio', 'entrenador', 'objeto'])
+    menus = MenusTuple(menu_inicio, menu_entrenador, menu_objetos)
 
-    # Testing area
-    print(menu_entrenador)
-    print(menu_inicio)
+    return liga, menus
 
 
-setup()
+def menu_objetos(menu_objetos, liga):
+    opcion = menu_objetos.seleccionar_opcion()
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
+    elif opcion == 5:
+        pass
+
+
+def menu_entrenador(menus, liga, indice_jugador):
+    accion = menus.entrenador.seleccionar_opcion()
+    if accion == 1:
+        pass
+    elif accion == 2:
+        liga.simular_ronda()
+    elif accion == 3:
+        liga.resumen_campeonato()
+    elif accion == 4:
+        menu_objetos(menus.objeto, liga)
+    elif accion == 5:
+        pass
+        opciones_menu_objeto = []
+        bayas = []
+        pociones = []
+        caramelos = []
+        for objeto in liga.entrenadores[indice_jugador].objetos:
+            if objeto.tipo == 'baya':
+                bayas.append(objeto)
+            elif objeto.tipo == 'pocion':
+                pociones.append(objeto)
+            elif objeto.tipo == 'caramelo':
+                caramelos.append(objeto)
+        [lista for lista in [bayas, pociones, caramelos] if len(lista) > 0]
+        if len(bayas) > 0:
+            opciones_menu_objeto.append()
+    elif accion == 6:
+        liga.entrenadores[indice_jugador].estado_entrenador()
+    elif accion == 7:
+        pass
+    elif accion == 8:
+        pass
+
+
+def main():
+    liga, menus = setup()
+    print('\nBienvenid@ al DCCampeonato Programon!\n')
+    indice_jugador = menus.inicio.seleccionar_opcion() - 1
+    print(f'Has seleccionado a: {liga.entrenadores[indice_jugador].nombre}')
+    menu_entrenador(menus, liga, indice_jugador)
+
+
+if __name__ == '__main__':
+    main()
--- Tareas/T1/menus.py
+++ Tareas/T1/menus.py
@@ -1,3 +1,4 @@
+from textwrap import indent
 from typing import List
 
 
@@ -5,13 +6,14 @@ class Menu():
     def __init__(self, header: str, opciones: List[str]) -> None:
         self.header = header
         self.opciones = opciones
-        self.opciones_dict = {a: self.opciones[a] for a in range(len(self.opciones))}
-        self.largos_opciones = [len(opciones[ind]) + 4 + ind // 10 for ind in range(len(opciones))]
-        self.ancho = max(self.largos_opciones)
+        # TODO: Si al final no se usa eliminar opciones_dict
+        self.opciones_dict = {a + 1: self.opciones[a] for a in range(len(self.opciones))}
+        self.len_opciones = [len(opciones[a]) + 4 + ((a + 1) // 10) for a in range(len(opciones))]
+        self.ancho = max(self.len_opciones)
         self.espacio_izq = int((self.ancho - len(self.header)) / 2)
         self.espacio_der = self.espacio_izq + 1
 
-    def seleccionar_opcion(self) -> str:
+    def seleccionar_opcion(self) -> int:
         opciones = self.opciones_dict.keys()
         valido = False
         while not valido:
@@ -22,13 +24,12 @@ class Menu():
             elif int(inp) not in opciones:
                 print(f'Las opciones solo son las siguientes: {list(opciones)}\n')
             else:
-                accion = self.opciones_dict[int(inp)]
                 valido = True
-        return accion
+        return int(inp)
 
     def __str__(self) -> str:
         menu_str = ' ' * self.espacio_izq + self.header + ' ' * self.espacio_der + '\n' \
                  + '─' * self.ancho + '\n'
-        for a in range(len(self.opciones)):
+        for a in range(1, len(self.opciones) + 1):
             menu_str += f'[{a}] {self.opciones_dict[a]}\n'
         return menu_str
