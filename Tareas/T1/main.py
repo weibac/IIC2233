16e59eda0187e4788f485800395d899fa3fe701c
weibac <milan.weibel@uc.cl>
2022-09-15 17:49:30 -0300

    refactor flujo y debug

--- Tareas/T1/main.py
+++ Tareas/T1/main.py
@@ -16,24 +16,42 @@ def menu_fin(menu_fin):
         salir()
 
 
+def revisar_volver_salir(menu):
+    opcion = menu.seleccionar_opcion() - 1
+    if opcion == len(menu.opciones) - 2:
+        return 'volver'
+    elif opcion == len(menu.opciones) - 1:
+        salir()
+    else:
+        return opcion
+
+
 def menu_entrenar(menu_programones, liga, ind_jug):
     if liga.entrenadores[ind_jug].energia < ENERGIA_ENTRENAMIENTO:  # TODO: segun wsp si no hay suficiente energ se gasta igual
         print(f'Entrenar a un programon cuesta {ENERGIA_ENTRENAMIENTO}\
  de energia pero solo tienes {liga.entrenadores[ind_jug].energia}')
     else:
-        indice_programon = menu_programones.seleccionar_opcion() - 1
-        liga.entrenadores[ind_jug].programones[indice_programon].entrenamiento()
-        liga.entrenadores[ind_jug].energia -= ENERGIA_ENTRENAMIENTO
+        opcion = revisar_volver_salir(menu_programones)
+        if opcion == 'volver':
+            pass
+        else:
+            indice_programon = opcion
+            liga.entrenadores[ind_jug].programones[indice_programon].entrenamiento()
+            liga.entrenadores[ind_jug].energia -= ENERGIA_ENTRENAMIENTO
 
 
 def menu_simular_ronda(menus, liga, indice_jugador):
-    indice_programon = menus.programones.seleccionar_opcion() - 1
-    usuario_perdio = liga.simular_ronda(indice_jugador, indice_programon)
-    if usuario_perdio:
-        print(f'Oh no! {liga.entrenadores[indice_jugador].nombre}, te han derrotado!\n')
-        menu_fin(menus.fin)
+    opcion = revisar_volver_salir(menus.programones)
+    if opcion == 'volver':
+        pass
     else:
-        print(f'{liga.entrenadores[indice_jugador].nombre}, has superado esta ronda! :D\n')
+        indice_programon = opcion
+        usuario_perdio = liga.simular_ronda(indice_jugador, indice_programon)
+        if usuario_perdio:
+            print(f'Oh no! {liga.entrenadores[indice_jugador].nombre}, te han derrotado!\n')
+            menu_fin(menus.fin)
+        else:
+            print(f'{liga.entrenadores[indice_jugador].nombre}, has superado esta ronda! :D\n')
 
 
 def menu_objetos(menu_objetos, liga, indice_jugador):
@@ -51,16 +69,14 @@ def menu_objetos(menu_objetos, liga, indice_jugador):
 
 
 def menu_sel_prog_obj(menu_programones, liga, ind_jug, ind_obj): # TODO: Volver solo un paso atrás
-    opcion = menu_programones.seleccionar_opcion() - 1
-    if opcion == len(menu_programones.opciones) - 2:
+    opcion = revisar_volver_salir(menu_programones)
+    if opcion == 'volver':
         pass
-    elif opcion == len(menu_programones.opciones) - 1:
-        salir()
     else:
         ind_prog = opcion
         objeto_seleccionado = liga.entrenadores[ind_jug].objetos[ind_obj]
         objeto_seleccionado.aplicar(liga.entrenadores[ind_jug].programones[ind_prog])
-
+        liga.entrenadores[ind_jug].objetos.remove(objeto_seleccionado)
 
 def menu_usar_obj(menu_programones, liga, ind_jug):
     # Construir menu lista objetos
@@ -70,13 +86,10 @@ def menu_usar_obj(menu_programones, liga, ind_jug):
     opciones_menu_usar_obj += OPCIONES_MENU_BASE
     menu_usar_obj = Menu(HEADER_MENU_USAR_OBJ, opciones_menu_usar_obj)
     # Usuario selecciona un objeto, volver o salir
-    opcion = menu_usar_obj.seleccionar_opcion() - 1
-    if opcion == len(opciones_menu_usar_obj) - 2:
+    opcion = revisar_volver_salir(menu_usar_obj)
+    if opcion == 'volver':
         pass
-    elif opcion == len(opciones_menu_usar_obj) - 1:
-        salir()
     else:
-        # Usuario selecciona un programon, volver o salir
         ind_obj = opcion - 1
         menu_sel_prog_obj(menu_programones, liga, ind_jug, ind_obj)
 
@@ -103,38 +116,8 @@ def menu_entrenador(menus, liga, indice_jugador):
             salir()
 
 
-def setup():
-    # Cargar archivos a base datos (named tuple)
-    entrenadores = cargar_archivo(RUTA_ENTRENADORES)
-    programones = cargar_archivo(RUTA_PROGRAMONES)
-    evoluciones = cargar_archivo(RUTA_EVOLUCIONES)
-    objetos = cargar_archivo(RUTA_OBJETOS)
-    DatosTuple = namedtuple('datos_archivos', ['entrens', 'progmnes', 'evols', 'objs'])  # TODO: Tal vez poner los nombres enteros
-    datos = DatosTuple(entrenadores, programones, evoluciones, objetos)
-
-    # Init liga
-    liga = LigaProgramon(datos)
-
-    # Crear menus fijos independientes de entrenador jugador
-    opciones_menu_inicio = []  # TODO: tal vez cargar desde datos en vez de liga
-    for entrenador in liga.entrenadores:
-        nombres_programones = [programon.nombre for programon in entrenador.programones]
-        opcion = f'{entrenador.nombre}: {", ".join(nombres_programones)}'
-        opciones_menu_inicio.append(opcion)
-    opciones_menu_inicio.append('Salir')
-    menu_inicio = Menu(HEADER_MENU_INICIO, opciones_menu_inicio)
-
-    menu_fin = Menu(HEADER_MENU_FIN_PARTIDA, OPCIONES_MENU_FIN_PARTIDA)
-    menu_entrenador = Menu(HEADER_MENU_ENTRENADOR, OPCIONES_MENU_ENTRENADOR)
-    menu_objetos = Menu(HEADER_MENU_OBJETOS, OPCIONES_MENU_OBJETOS)
-
-    # Empaquetar menus
-    menus = (menu_inicio, menu_fin, menu_entrenador, menu_objetos)
-
-    return liga, menus
-
-
 def menu_inicio(menus, liga):
+
     # Jugador selecciona su entrenador
     opcion = menus[0].seleccionar_opcion()
     if opcion == len(menus[0].opciones):
@@ -156,11 +139,45 @@ def menu_inicio(menus, liga):
 
     menu_entrenador(menus, liga, indice_jugador)
 
+def cargar_datos():
+    # Cargar archivos a base datos (named tuple)
+    entrenadores = cargar_archivo(RUTA_ENTRENADORES)
+    programones = cargar_archivo(RUTA_PROGRAMONES)
+    evoluciones = cargar_archivo(RUTA_EVOLUCIONES)
+    objetos = cargar_archivo(RUTA_OBJETOS)
+    DatosTuple = namedtuple('datos_archivos', ['entrens', 'progmnes', 'evols', 'objs'])  # TODO: Tal vez poner los nombres enteros
+    datos = DatosTuple(entrenadores, programones, evoluciones, objetos)
+    return datos
+
+
+def menus_fijos(liga):
+    # Crear menus fijos independientes de entrenador jugador
+    opciones_menu_inicio = []  # TODO: tal vez cargar desde datos en vez de liga
+    for entrenador in liga.entrenadores:
+        nombres_programones = [programon.nombre for programon in entrenador.programones]
+        opcion = f'{entrenador.nombre}: {", ".join(nombres_programones)}'
+        opciones_menu_inicio.append(opcion)
+    opciones_menu_inicio.append('Salir')
+    menu_inicio = Menu(HEADER_MENU_INICIO, opciones_menu_inicio)
+
+    menu_fin = Menu(HEADER_MENU_FIN_PARTIDA, OPCIONES_MENU_FIN_PARTIDA)
+    menu_entrenador = Menu(HEADER_MENU_ENTRENADOR, OPCIONES_MENU_ENTRENADOR)
+    menu_objetos = Menu(HEADER_MENU_OBJETOS, OPCIONES_MENU_OBJETOS)
+
+    # Empaquetar menus
+    menus = (menu_inicio, menu_fin, menu_entrenador, menu_objetos)
+
+    return menus
+
 
 def main():
+    datos = cargar_datos()
     print('\nBienvenid@ al DCCampeonato Programon!\n')
-    liga, menus = setup()
+    liga = LigaProgramon(datos)
     while liga.ronda_actual < 4:
+        # TODO: La liga se reinicia cada vez que se llega al menu incio
+        #liga = LigaProgramon(datos)
+        menus = menus_fijos(liga)
         menu_inicio(menus, liga)
 
 
