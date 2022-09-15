b92c01667b124fdfff970ab2e3a19d82b8efd7b0
weibac <milan.weibel@uc.cl>
2022-09-15 15:00:04 -0300

    centro urbano

--- Actividades/AS2/centro_urbano.py
+++ Actividades/AS2/centro_urbano.py
@@ -10,6 +10,30 @@ class CentroUrbano:
         self.chozas = 0
         # Completar
 
+    lock_oro = Lock()
+
+    @property
+    def oro(self, lock_oro):
+        with lock_oro:
+            return self.oro
+
+    @oro.setter
+    def oro(self, value, lock_oro):
+        with lock_oro:
+            self.oro = value
+
+    lock_chozas = Lock()
+
+    @property
+    def chozas(self, lock_chozas):
+        with lock_chozas:
+            return self.chozas
+
+    @chozas.setter
+    def chozas(self, value, lock_chozas):
+        with lock_chozas:
+            self.chozas = value
+
     @property
     def barbaros(self) -> int:
         return int(self.chozas * PONDERADOR_BARBAROS)
