7af1f64c04853fca1d32b4fe0295c0cfa5ae0f48
weibac <milan.weibel@uc.cl>
2022-10-05 20:05:24 -0300

    fix parametros: rutas

--- Tareas/T2/parametros.py
+++ Tareas/T2/parametros.py
@@ -1,3 +1,5 @@
+import os
+
 # Los intervalos están en milisegundos
 INTERVALO_DISPARO = 2000 
 INTERVALO_SOLES_GIRASOL = 20000
@@ -37,3 +39,13 @@ COSTO_PAPA = 75
 # Caracteres de nombre usuario
 MIN_CARACTERES = 3
 MAX_CARACTERES = 15
+
+# RUTAS
+# uis
+RUTA_UI_VENTANA_INICIO = os.path.join('frontend', 'ventana_inicio.ui')
+RUTA_UI_VENTANA_SELECCION_ESCENARIO = os.path.join('frontend', 'ventana_seleccion_escenario.ui')
+RUTA_UI_VENTANA_JUEGO = os.path.join('frontend', 'ventana_juego.ui')
+# fondos
+RUTA_FONDO_DIA = os.path.join('sprites', 'Fondos', 'jardinAbuela.png')
+RUTA_FONDO_NOCHE = os.path.join('sprites', 'Fondos', 'salidaNocturna.png')
+RUTA_FONDO_MENU = os.path.join('sprites', 'Fondos', 'fondoMenu.png')
